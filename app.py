e4e0847fc76c35d22a393ee35d49b99a763c019f
jamiebird119 <jamie.bird119@gmail.com>
2020-06-25 10:52:54 +0000

    Add task function completed with submit button and test function added

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ app.config["MONGO_URI"] = 'mongodb+srv://route:rootUser@myfirstcluster-uwurq.mon
 mongo = PyMongo(app)
 
 
-
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
@@ -21,7 +20,17 @@ def get_tasks():
 
 @app.route("/add_task")
 def add_task():
-    return render_template("addtask.html")
+    _categories = mongo.db.categories.find()
+    category_list = [category for category in _categories]
+    return render_template("addtask.html",
+                           categories=category_list)
+
+
+@app.route("/insert_task", methods=["POST"])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
